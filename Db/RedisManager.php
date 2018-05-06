5f3681e83ceffa4151ec35c01bafc21417aa74e0
t3ran13 <teran-555@yandex.ru>
2018-05-06 16:56:17 +0300

    upd RedisManager

--- Db/RedisManager.php
+++ Db/RedisManager.php
@@ -1,10 +1,57 @@
 <?php
 
 
-
 namespace MyApp\Db;
 
 
 class RedisManager extends \GolosPhpEventListener\app\db\RedisManager
 {
+    /**
+     * @param array $data
+     *
+     * @return int the length of the list after the push operation.
+     */
+    public function ratingPostRewardAddToQueue($data)
+    {
+        $status = $this->connect->rPush("myapp:rewards-post-list", json_encode($data));
+
+        return $status;
+    }
+
+    /**
+     * @return int
+     */
+    public function ratingPostRewardGetQueueLength()
+    {
+        return $this->connect->llen("myapp:rewards-post-list");
+    }
+
+    /**
+     * @return bool|array as key => json, if nothing return false
+     */
+    public function ratingPostRewardGetFirstFromQueue()
+    {
+        $json = $this->connect->lIndex("myapp:rewards-post-list", 0);
+        return $json === null ? false : json_decode($json, true);
+    }
+
+    /**
+     * @return bool
+     */
+    public function ratingPostRewardRemoveFirstFromQueue()
+    {
+        return $this->connect->lPop("myapp:rewards-post-list") === false ? false : true;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return int the length of the list after the push operation.
+     */
+    public function ratingUsersRewardAddToQueue($data)
+    {
+        $status = $this->connect->rPush("myapp:rewards-users-list", json_encode($data));
+
+        return $status;
+    }
 }
