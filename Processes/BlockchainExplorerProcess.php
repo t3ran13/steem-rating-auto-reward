6daa17429f508fde771a98a1fae8b39da4dfef2c
t3ran13 <teran-555@yandex.ru>
2018-05-21 10:54:42 +0300

    set 40 blocks latency for block chain explorer

--- Processes/BlockchainExplorerProcess.php
+++ Processes/BlockchainExplorerProcess.php
@@ -31,6 +31,6 @@ class BlockchainExplorerProcess extends \GolosPhpEventListener\app\process\Block
     {
         $currentBlockNumber = parent::getCurrentBlockNumber();
 
-        return $currentBlockNumber - 20;
+        return $currentBlockNumber - 40;
     }
 }
