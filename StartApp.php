94f4b6da4971b878e26a0dbb8b60c18080192890
t3ran13 <teran-555@yandex.ru>
2018-06-06 16:43:48 +0300

    GMT time by default

--- StartApp.php
+++ StartApp.php
@@ -20,6 +20,8 @@ use MyApp\Processes\RatingRewardUsersSenderProcess;
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+date_default_timezone_set('GMT');
+
 define('PATH', __DIR__);
 require __DIR__ . "/Autoloader.php"; // only in GrapheneNodeClient project
 require __DIR__ . '/vendor/autoload.php';
